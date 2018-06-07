20709e4ade6c36806287c4eaf61dc4cf865e4d53
Oriele1982 <oriele.no@hotmail.com>
2018-06-06 21:30:15 -0400

    tweeter terminado cumple con el click

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,37 +12,40 @@ $(document).ready(function(){
 		+ '</div>'
 		+ '<ul class="tweet_select>'
 		+ '<li class="tweet_item"><i class ="likeable fas fa-heart"></i></li>' 
-		+ '<li class="tweet_item"><span id = "counter">0</span></li>'
+		+ '<li class="tweet_item"><span class = "counter">0</span></li>'
 		+ '<li class="tweet_item"><i class ="fas fa-trash"></i></li>'
 		+ '</div>');
 		$("#tweet_textarea").val('');
 		$("#tweet_textarea").focus();
 
-	
-	// Clase likeable (corazón) cambia de color con un click y aumenta el contador en 1
-		$(".likeable").on("click", function(event){
-			event.preventDefault();
-			event.stopPropagation();
-			$(this).toggleClass("likeable--red");
 
-				var i= 0
-				if($(this).hasClass("likeable--red")){
-					$("#counter").text(i++); 
-				
-				} else {
-					$("#counter").text(i);
-				}; 
-
-			document.getElementById("counter").innerHTML = i++;
-		});
-
-		
-
-		// Icono basurero borra el post
+			// Icono basurero borra el post
 
 			$(".fa-trash").on("click", function(){   
 				$(this).parent().parent().parent().remove();
 			});
 
+
 	});
+	
+	// Clase likeable (corazón) cambia de color con un click y aumenta el contador en 1
+		$('.tweet_col').on("click", ".likeable", function(event){
+			var number = $(this).siblings('.tweet_item').children().text();
+			console.log(number);
+			number = parseInt(number);
+			console.log(number);
+
+			if($(this).hasClass("likeable--red")){
+				number -= 1;
+			} else {
+				number += 1;
+			};	
+
+			$(this).toggleClass("likeable--red");
+			$(this).siblings('.tweet_item').children(".counter").text(number);
+				 
+
+			
+		});
+	
 });
